fd8958b5fb728a6fd788e4c89649c21bcb58e478
Kevin Behrens <kevin@thekbb.net>
2017-04-07 11:50:24 -0500

    fix rvm path

--- env.zsh
+++ env.zsh
@@ -1,10 +1,9 @@
 export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 export GRADLE_OPTS="-Xms2048m -Xmx4096m"
-export PATH=$PATH:~/.rvm/gems/ruby-2.2.1/bin
 export PATH=$PATH:/usr/local/opt/go/libexec/bin
 export GOPATH=$HOME/gopath
 export PATH=$PATH:$GOPATH/bin
 export PATH=$HOME/.fastlane/bin:$PATH
-
+export PATH="$PATH:$HOME/.rvm/bin"
 eval "$(thefuck --alias)"
 
