c520202a42a57eaddcb0b9639df7bf645307f678
Kevin Behrens <kevin@thekbb.net>
2017-08-29 09:07:39 -0500

    fully break up with cowsay - no need to random cow with parrot say

--- misc.zsh
+++ misc.zsh
@@ -1,3 +1,3 @@
 # Placeholder for misc configuration
-fortune overheard-at-target|parrotsay -f $(ls /usr/local/Cellar/cowsay/3.03/share/cows/*.cow | gshuf -n1)
+fortune overheard-at-target|parrotsay
 
