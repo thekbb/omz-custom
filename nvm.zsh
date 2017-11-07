2fb46feae2f5eaa10b8ed34db7edbf4c8b52c90f
Kevin Behrens <kevin@thekbb.net>
2017-11-07 16:18:46 -0600

    Define nvm if not defined
    Fixes #1

--- nvm.zsh
+++ nvm.zsh
@@ -1,3 +1,9 @@
+# Set NVM_DIR if it isn't already defined
+[[ -z "$NVM_DIR" ]] && export NVM_DIR="$HOME/.nvm"
+
+# Load nvm if it exists
+[[ -f "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
+
 autoload -Uz add-zsh-hook
 load-nvmrc() {
   if [[ -f .nvmrc && -r .nvmrc ]]; then
