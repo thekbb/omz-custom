9ef18ee18d0ce6608be2f98d01a0b033a81895d9
Kevin Behrens <kevin@thekbb.net>
2017-11-13 09:22:25 -0600

    make sure parrotsay has node

--- misc.zsh
+++ misc.zsh
@@ -1,3 +1,8 @@
 # Placeholder for misc configuration
+# Set NVM_DIR if it isn't already defined
+[[ -z "$NVM_DIR" ]] && export NVM_DIR="$HOME/.nvm"
+
+# Load nvm if it exists
+[[ -f "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
 fortune overheard-at-target|parrotsay
 
