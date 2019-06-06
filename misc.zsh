0d9cbe00fe475432391668fc227cc3af9596771c
Kevin Behrens <kevin@thekbb.net>
2019-06-06 09:25:12 -0500

    rework nvm

--- misc.zsh
+++ misc.zsh
@@ -2,7 +2,12 @@
 # Set NVM_DIR if it isn't already defined
 [[ -z "$NVM_DIR" ]] && export NVM_DIR="$HOME/.nvm"
 
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
+
+
 # Load nvm if it exists
 [[ -f "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
 fortune | parrotsay
 
+eval $(thefuck --alias)
