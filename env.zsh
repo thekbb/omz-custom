34436ec6dc69d92fa2e561c42cc278bf1b0cf6ca
Kevin Behrens <kevin@thekbb.net>
2019-12-31 09:20:06 -0600

    add homebrew's sbin and remove old ruby gems dir from PATH

--- env.zsh
+++ env.zsh
@@ -1,4 +1,4 @@
-export PATH=/Users/z013w36/.rvm/gems/ruby-2.4.1/bin:$PATH
+export PATH="/usr/local/sbin:$PATH
 export GRADLE_OPTS="-Xms2048m -Xmx4096m"
 export PATH=$PATH:/usr/local/opt/go/libexec/bin
 export GOPATH=$HOME/gopath
