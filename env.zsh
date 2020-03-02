9053099710ec8099f6eaa2835be5161bad0f1327
Kevin Behrens <kevin@thekbb.net>
2020-03-02 14:50:49 -0600

    Break up wwith rvm.  long live rbenv!

--- env.zsh
+++ env.zsh
@@ -4,7 +4,6 @@ export PATH=$PATH:/usr/local/opt/go/libexec/bin
 export GOPATH=$HOME/gopath
 export PATH=$PATH:$GOPATH/bin
 export PATH=$HOME/.fastlane/bin:$PATH
-export PATH="$PATH:$HOME/.rvm/bin"
 export ANDROID_HOME=$HOME/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/platform-tools
@@ -14,7 +13,5 @@ export PATH=/usr/local/opt/sqlite/bin:$PATH
 export ANDROID_SDK=$HOME/Library/Android/sdk
 export PATH=$ANDROID_SDK/emulator:$ANDROID_SDK/tools:$PATH
 
-# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
-export PATH="$PATH:$HOME/.rvm/bin"
-
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+eval "$(rbenv init -)"
+export PATH="$HOME/.rbenv/shims:$PATH"
