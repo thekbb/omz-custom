9232ba62bf0250a8cd1d999b4ba09ad0781d50eb
Kevin Behrens <kevin@thekbb.net>
2020-09-01 10:02:28 -0500

    add tfswitcher

---
+++ tfswitch.zsh
@@ -0,0 +1,9 @@
+autoload -Uz add-zsh-hook
+load-tfswitch() {
+  command -v tfswitch &> /dev/null || { echo >&2 "tfswitch not found.  Aborting."; exit 1; }
+  if [[ -f main.tf || -f version.tf ]]; then
+    tfswitch
+  fi
+}
+add-zsh-hook chpwd load-tfswitch
+load-tfswitch
