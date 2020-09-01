470249dfb4008ec184a54978a7ebf6b302b8d094
Kevin Behrens <kevin@thekbb.net>
2020-09-01 10:04:05 -0500

    ONE SPACE

--- tfswitch.zsh
+++ tfswitch.zsh
@@ -1,6 +1,6 @@
 autoload -Uz add-zsh-hook
 load-tfswitch() {
-  command -v tfswitch &> /dev/null || { echo >&2 "tfswitch not found.  Aborting."; exit 1; }
+  command -v tfswitch &> /dev/null || { echo >&2 "tfswitch not found. Aborting."; exit 1; }
   if [[ -f main.tf || -f version.tf ]]; then
     tfswitch
   fi
