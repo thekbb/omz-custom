3182d79f067a4b26da0eebd179d5aeb459b134c4
Kevin Behrens <kevin@thekbb.net>
2020-12-10 12:05:58 -0600

    fix tfswith function to not kill shell if tfswitch isnt found

--- tfswitch.zsh
+++ tfswitch.zsh
@@ -1,6 +1,6 @@
 load-tfswitch() {
-  command -v tfswitch &> /dev/null || { echo >&2 "tfswitch not found. Aborting."; exit 1; }
   if [[ -f main.tf || -f version.tf ]]; then
+    command -v tfswitch &> /dev/null || { echo >&2 "tfswitch not found, not auto-switching."; return 1; }
     tfswitch
   fi
 }
